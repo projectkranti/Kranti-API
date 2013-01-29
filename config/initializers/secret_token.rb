a40bc643e7da3c9858ccc37d5d218d55648cc47d
Piyush <piyushmnimbalkar@gmail.com>
2013-01-29 18:32:45 +0530

    [Rohini/Piyush] Adding rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KrantiApi::Application.config.secret_token = '9927fbb2295bc2631ef04b7cfd229ecf41d17fec1e232140f86d0f613cd7d889eda0fe9db522fa8306a93560bbbd2a2951978a75c504e71bfcea20c0a8722bc9'
