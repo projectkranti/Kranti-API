d7eb35f8996108edc09882a8076bc34e7ec19818
Mohit Thatte <mohit.thatte@thoughtworks.com>
2013-04-23 11:51:23 +0000

    Mohit : Add user model with uuid and reference from issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130129140347) do
+ActiveRecord::Schema.define(:version => 20130423112251) do
 
   create_table "issues", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(:version => 20130129140347) do
     t.string   "location",    :limit => nil
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
+    t.integer  "user_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string "uuid"
   end
 
 end
